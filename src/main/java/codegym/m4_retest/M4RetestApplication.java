88abf94f27e74824afc6d72ce0d5d6913e9a6ab0
Hoang Duy <duy.nghoang526@gmail.com>
2024-12-17 17:53:22 +0700

    first commit

---
+++ src/main/java/codegym/m4_retest/M4RetestApplication.java
@@ -0,0 +1,13 @@
+package codegym.m4_retest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M4RetestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M4RetestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/codegym/m4_retest/M4RetestApplicationTests.java
@@ -0,0 +1,13 @@
+package codegym.m4_retest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M4RetestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
